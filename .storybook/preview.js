e44b5be9c176c2864dc3c906296fe0e191059014
Brian <brianc.contacto@gmail.com>
2022-09-06 13:53:39 -0400

    fix css tokens import

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,3 @@
-import '../src/index.css';
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
