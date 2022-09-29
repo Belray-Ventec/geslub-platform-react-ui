4383d96f3fa75b1fe4a9965c2b45fc30a231873b
Brian <brianc.contacto@gmail.com>
2022-09-29 17:00:04 -0300

    comments removed

--- stories/atoms/titles/H1.stories.tsx
+++ stories/atoms/titles/H1.stories.tsx
@@ -2,24 +2,21 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { H1 } from '../../../src/components/atoms/h1';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'atoms/titles/H1',
   component: H1,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     color: { control: 'color' },
     size: { control: 'select' },
   },
 } as ComponentMeta<typeof H1>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof H1> = (args) => (
   <H1 {...args}>{args.children}</H1>
 );
 
 export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
 Default.args = {
   children: 'Documentos Técnicos',
   size: 'h1',
--- stories/pages/Tokens.stories.tsx
+++ stories/pages/Tokens.stories.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { TokenPage } from '../../src/components/pages/tokensPage/';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Pages/TokenPage',
   component: TokenPage,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } as ComponentMeta<typeof TokenPage>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof TokenPage> = (args) => <TokenPage />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'TokenPage',
-};
