0fd42f8e37f9564d7c01886c4927f3c825e726bc
Brian <brianc.contacto@gmail.com>
2023-01-03 13:30:38 -0300

    install chakraui

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,30 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+  core: {
+    builder: 'webpack5',
+  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-css-modules',
+    '@chakra-ui/storybook-addon',
   ],
+  features: {
+    emotionAlias: false,
+  },
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
+    reactDocgen: false
   },
   staticDirs: ['../src/public'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    })
+
+    return config
+  },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,12 @@
 import '../src/public/preview.css';
+import { geslubTheme } from '../src/utils/theme'
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
+  chakra: {
+    theme: geslubTheme,
+  },
 };
 
 export const decorators = [
